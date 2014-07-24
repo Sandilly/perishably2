f1565fc47855ad5ef3fd2d0871eda3b36e9f11ce
April Rabkin <april.rabkin@gmail.com>
2014-07-24 16:42:15 -0400

    working

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140723150742) do
   create_table "product_recipients", force: true do |t|
     t.integer "user_added_product_id"
     t.integer "recipient_id"
+    t.integer "phone_number"
   end
 
   create_table "products", force: true do |t|
