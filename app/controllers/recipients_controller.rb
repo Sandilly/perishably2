b399a6c250ebe2bb28db7888911b7f5d05976e81
Hannah Kim <hrk624@gmail.com>
2014-07-25 09:44:57 -0400

    layout changes probably similar to master

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -6,22 +6,16 @@ class RecipientsController < ApplicationController
       @recipient = Recipient.new
   end
 
-  # def create
-  #     user_product = UserAddedProduct.find(params[:user_added_product_id])
-  #     binding.pry
-  #     if recipient = Recipient.find_by(email: params[:email])
-  #       user_product.recipients << recipient
-  #     else
-  #       recipient = Recipient.new(recipient_params)
-  #       user_product.recipients << recipient
-  #     end
-  #   if recipient.save
-  #     redirect_to user_added_products_path
-  #   else
-  #     flash.now[:notice] = "Your submission is invalid."
-  #     render "new"    
-  #   end
-  # end
+  def create
+    @product = UserAddedProduct.find(params[:recipient][:user_added_product_id])
+    @recipient = Recipient.new(recipient_params)
+    if @recipient.save
+      current_user.recipients << recipient
+      redirect_to user_added_products_path, notice:  "#{@recipient.name}.capitalize will be notified on #{@product.exp_date.strftime("%B %d, %Y")} about #{@user_product.name}."
+    else 
+      render "user_added_products/show"
+    end
+  end  
 
   def show
     @recipients_stuff = @recipient.user_added_products
