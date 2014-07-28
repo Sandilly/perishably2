bf7416f2c0106c399be238e4626c6f7d36f0d90b
Sandy <Sandy.L45@gmail.com>
2014-07-28 17:56:47 -0400

    clean code for working dynamic exp date generator

--- app/assets/javascripts/user_added_products.js
+++ app/assets/javascripts/user_added_products.js
@@ -23,58 +23,6 @@ $(function() {
 	});
 });
 
-// var getDate = $(function(){
-// 	var curr_date = new Date();
-// 	var curr_day = curr_date.getDate();
-// 	var curr_month = curr_date.getMonth() + 1;
-// 	var curr_year = curr_date.getFullYear();
-
-// 	if(curr_day < 10){
-// 		curr_day = "0" + curr_day
-// 	};
-
-// 	if(curr_month < 10){
-// 		curr_month = "0" + curr_month
-// 	};
-// });
-
-// $(function(){
-// 	return $('#user_added_product_number_unit_of_time').change(function(){
-// 		var num_time = parseInt($(this).val());
-// 		change_exp(num_time);
-// 	});
-// });
-
-
-// function change_exp(num_time){
-// 	// $("#user_added_product_number_unit_of_time, #user_added_product_unit_of_time_period")
-// 	$('#user_added_product_unit_of_time_period').change(function(){
-// 			var period_time = $(this).val();
-// 			switch (period_time){
-// 		   	case "Day(s)":
-	
-// 		   		$('#datepicker').val((moment().add('d', num_time).format("L")));
-// 		   		break;
-// 		   	case "Week(s)":
-// 		   		$('#datepicker').val((moment().add('w', num_time).format("L")));
-// 		   		break;
-// 		   	case "Month(s)": 
-// 		      $('#datepicker').val((moment().add('M', num_time).format("L")));
-// 		      break;
-// 		    case "Year(s)": 
-// 		      $('#datepicker').val((moment().add('y', num_time).format("L")));
-// 		      break;
-
-// 		   	default: 
-// 		      alert('Default case');
-// 		      break;
-// 			}
-// 			//$('#datepicker').val(date);	
-// 		});
-// };
-
-
-
 function change_exp(){
 	$("#user_added_product_number_unit_of_time, #user_added_product_unit_of_time_period").change(function(){
 	//$('#user_added_product_unit_of_time_period').change(function(){
@@ -98,9 +46,8 @@ function change_exp(){
 		   	default: 
 		      alert('Default case');
 		      break;
-			};
-			//$('#datepicker').val(date);	
-		});
+		};
+	});
 };
 
 
