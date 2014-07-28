4dce62151514696343f9790e1f7aefa5e2ddd9e6
Sandy <Sandy.L45@gmail.com>
2014-07-28 18:02:09 -0400

    rebase

--- app/assets/javascripts/user_added_products.js
+++ app/assets/javascripts/user_added_products.js
@@ -1,5 +1,4 @@
 $(function() {
-	console.log("hello");
 	return $("#user_added_product_name").autocomplete({
 		source: $("#user_added_product_name").data("autocomplete-source"),
 		minLength: 3,
@@ -11,7 +10,6 @@ $(function() {
 				$('#user_added_product_unit_of_time_period').val(data.unit_of_time_period);
 				$('#user_added_product_number_unit_of_time').val(data.number_unit_of_time);
 				var date =	moment().add(data.unit_of_time_period.replace("(s)", ""), data.number_unit_of_time).format('YYYY-MM-DD');
-				console.log(date);
 				$('#datepicker').val(date);	
 			});
 		}
@@ -23,5 +21,15 @@ $(function() {
 		minDate: new Date(2014, 6, 23)
 	});
 });
+
+
+$(function(){
+	return $('#user_added_product_number_unit_of_time').change(function(){
+		var num_time = parseInt($(this).val());
+		alert(num_time);
+	});
+});
+
+
 
 
