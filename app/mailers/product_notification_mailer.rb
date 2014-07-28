6c693f4216187acd30c5903485268356c6c762ca
April Rabkin <april.rabkin@gmail.com>
2014-07-28 19:53:04 -0400

    added product name and exp date to email

--- app/mailers/product_notification_mailer.rb
+++ app/mailers/product_notification_mailer.rb
@@ -1,11 +1,10 @@
 class ProductNotificationMailer < ActionMailer::Base
     
     default :from => "perishably@gmail.com" #fluffhead is the password
-  
-  def notification_for(product)
-    @product = product
 
-    mail(:to => @product.user.email, :subject => "Perishably - Product Expiration Notice")
+  def notification_for(product)
+  	@product = product 
+    mail(:to => @product.user.email, :subject => "Perishab.ly alert regarding your #{@product.name}")
   end
 
   def test_email_for(email)
