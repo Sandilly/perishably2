f1e1c6737ca171d865962dad9578fddb0bc3f49c
Hannah Kim <hrk624@gmail.com>
2014-07-30 11:46:20 -0400

    deleted lots of commented out code

--- app/models/cosmetic_scraper.rb
+++ app/models/cosmetic_scraper.rb
@@ -117,14 +117,9 @@ scraper.parse('.tiptable tr') do |element|
     end
   end
   makeup_array << item
-  
-
 end
 
-
 makeup_array.each do |element|
-  #[{:name => "mascara",:expiration => "12 months"}]
-  # {:name => "mascara",:expiration => "12 months"}
   product = Product.new
 
   product.name = element[:name]
@@ -134,40 +129,3 @@ makeup_array.each do |element|
   
   product.save
 end
-
-
-
-# require_relative 'scraper.rb'
-# require 'nokogiri'
-# require 'rest-client'
-# require 'pry'
-
-
-# makeup_array= []
-# scraper = Scraper.new("www.bellasugar.com/When-Throw-Makeup-Away-Guidelines-Cosmetic-Life-Span-1124422")
-# scraper.fetch
-
-# scraper.parse('.tiptable tr') do |element|
-#   item = {}
-#   next if element.css('td').empty?
-#   item[:name] =  element.css('td')[0].text
-#   item[:expiration] = element.css('td')[1].text
-#   makeup_array << item
-# end
-
-# # store = YAML::Store.new "cosmetic.store"
-
-# # store.transaction do
-# #   store["cosmetics"] = @makeup_hash
-# # end  
-
-# # f = File.open("cosmetic.store", "r")
-
-# makeup_array.each do |element|
-#   element.each do |key, value|
-#     product = Product.new
-#     product.name = key
-#     product.expiration_date = value
-#     product.save
-#   end
-# end
