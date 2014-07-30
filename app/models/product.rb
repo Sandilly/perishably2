245b2209d48003a2c9fede3b9948bdfda1b111f8
Hannah Kim <hrk624@gmail.com>
2014-07-30 11:46:20 -0400

    deleted lots of commented out code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,7 @@
 class Product < ActiveRecord::Base
-
   validates :name, presence: true, uniqueness: true
-  # validates :unit_of_time_period, presence: true
 
   def self.search(query)
     where("name like ?", "%#{query}%") 
   end
-
-#below are methods for the autofill:
-
-
-#above are methods for the autofill
-
 end
--- app/models/user_added_product.rb
+++ app/models/user_added_product.rb
@@ -4,8 +4,6 @@ class UserAddedProduct < ActiveRecord::Base
   validates :name, :presence => true
   accepts_nested_attributes_for :recipients
 
-  #before_save :set_expiration_date #, :set_notification_date
-
   def notify
     if self.user.notify?
       ProductNotificationMailer.notification_for(self).deliver
@@ -16,10 +14,6 @@ class UserAddedProduct < ActiveRecord::Base
     
     time_add = self.number_unit_of_time.to_i
     time_type = self.unit_of_time_period
-    
-    # product_date_now = DateTime.now
-
-    # product_date_now = self.created_at || DateTime.now
     
     if time_type =~ /\bday(|\(s\))?/i
       self.exp_date = product_date_now + time_add.days
@@ -66,48 +60,4 @@ class UserAddedProduct < ActiveRecord::Base
       self.notification_date = product_date_exp
     end
   end
-
-
-#progress bar
-  # def time_from_expiration
-  #   if DateTime.now < self.exp_date
-  #     ( (self.exp_date - DateTime.now).to_i / (self.notification_date - DateTime.now).to_i )
-  #   end
-  # end
-
-
-  # def exp_date(user_found)
-  #   @time_add = @user_product.number_unit_of_time.to_i
-  #   time_type = @user_product.unit_of_time_period
-
-  #   @product_exp = @user_product.created_at
-    
-  #   if @time_type =~ /\bday(s|\(s\))?/i
-  #     @exp_date = @product_exp + @time_add.days
-  #   elsif @time_type =~ /\bweek(s|\(s\)?)/i
-  #     @exp_date = @product_exp + @time_add.weeks
-  #   elsif @time_type =~ /\bmonth(s|\(s\)?)/i
-  #     @exp_date = @product_exp + @time_add.months
-  #   elsif @time_type =~ /\byear(s|\(s\)?)/i
-  #     @exp_date = @product_exp + @time_add.years
-  #   else
-  #     @exp_date = @user_product.unit_of_time_period
-  #   end
-  # end
-
-  
 end
-
-
-
-# product_date_now + time_add.days
-# product_date_now.+(time_add.days)
-# product_date_now.send(:+, time_add.days)
-# product_date_now.send(:+, time_add.send(:days))
-
-# def change_days(operator, unit_of_time)
-#   product_date_now.send(operator, time_add.send(unit_of_time))
-# end
-
-# change_days(:+, :days)
-# change_days(:-, :weeks)
