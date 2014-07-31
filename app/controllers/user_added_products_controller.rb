3daf9c1c3b12a290c4c20769ae7b81a64207a627
Sandy <Sandy.L45@gmail.com>
2014-07-31 13:56:54 -0400

    Cleaned erb files and mirroring update view

--- app/controllers/user_added_products_controller.rb
+++ app/controllers/user_added_products_controller.rb
@@ -63,18 +63,6 @@ class UserAddedProductsController < ApplicationController
 
     @product_exp = @user_product.created_at
     
-    # if @time_type =~ /\bday(s|\(s\))?/i
-    #   @exp_date = @product_exp + @time_add.days
-    # elsif @time_type =~ /\bweek(s|\(s\)?)/i
-    #   @exp_date = @product_exp + @time_add.weeks
-    # elsif @time_type =~ /\bmonth(s|\(s\)?)/i
-    #   @exp_date = @product_exp + @time_add.months
-    # elsif @time_type =~ /\byear(s|\(s\)?)/i
-    #   @exp_date = @product_exp + @time_add.years
-    # else
-    #   @exp_date = @user_product.unit_of_time_period
-    # end
-
   end
 
   private
