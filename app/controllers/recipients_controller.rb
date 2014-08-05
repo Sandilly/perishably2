2d393759aa7048d462e6dbfcd983b63787621ff9
Hannah Kim <hrk624@gmail.com>
2014-08-05 16:59:40 -0400

    fixed weird html code stuff that came out on edit page, fixed edit recipient and delete recipient from uap page

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,5 +1,4 @@
 class RecipientsController < ApplicationController
-  belongs_to :user
   before_action :find_objects, only: [:show]
 
   def new
@@ -12,9 +11,20 @@ class RecipientsController < ApplicationController
     @recipient = Recipient.new(recipient_params)
     if @recipient.save
       current_user.recipients << @recipient
-      redirect_to user_added_products_path, notice:  "#{@recipient.name}.capitalize will be notified on #{@product.exp_date.strftime("%B %d, %Y")} about #{@user_product.name}."
+      redirect_to user_added_products_path
     else
       render "user_added_products/show"
+    end
+  end
+
+  def update
+    @user_product = UserAddedProduct.find(params[:user_added_product_id])
+    @recipient = Recipient.find(params[:id])
+    @recipient.update(recipient_params)
+    if @recipient.save  
+      redirect_to user_added_product_path(@user_product), notice:  "#{@recipient.name.capitalize} has been updated."
+    else
+      render :edit
     end
   end
 
@@ -28,6 +38,18 @@ class RecipientsController < ApplicationController
     end
   end
 
+  def edit
+    @user_product = UserAddedProduct.find(params[:user_added_product_id])
+    @recipient = Recipient.find(params[:id])
+  end
+
+  def destroy
+    @user_product = UserAddedProduct.find(params[:user_added_product_id])
+    @recipient = Recipient.find(params[:id])
+    @recipient.destroy
+    redirect_to user_added_products_path
+  end
+
   private
 
   def find_objects
@@ -35,7 +57,7 @@ class RecipientsController < ApplicationController
     @recipient = Recipient.find(params[:id])
   end
 
-  def product_params
-    params.require(:user_added_product).permit(:name, :email, :notification_date, :sms, :product_details, :unit_of_time_period, :number_unit_of_time, :exp_date, :storage, :recipients_attributes =>[:id, :name, :email, :phone_number])
+  def recipient_params
+    params.require(:recipient).permit(:name, :email, :phone_number)
   end
 end
