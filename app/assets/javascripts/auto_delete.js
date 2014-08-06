dfa5cde2fd0ff53bbc78741e166124db64a35616
Sandy <Sandy.L45@gmail.com>
2014-08-06 16:41:08 -0400

    fixed merge conflicts working copy

--- app/assets/javascripts/auto_delete.js
+++ app/assets/javascripts/auto_delete.js
@@ -1,6 +1,6 @@
-function autoDelete(){
-  var product_doe = $('#user_product_list .product_info .each_product_info .product_doe').text();
+// function autoDelete(){
+//   var product_doe = $('#user_product_list .product_info .each_product_info .product_doe').text();
 
   
-  alert(moment(product_doe));
-};
+//   alert(moment(product_doe));
+// };
