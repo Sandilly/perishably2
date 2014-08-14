c962375551b56df539f235a8b06bccf9dc3ff207
Sandy <Sandy.L45@gmail.com>
2014-08-14 13:52:16 -0400

    added twilio changes

--- app/models/twilio_client.rb
+++ app/models/twilio_client.rb
@@ -9,11 +9,11 @@ class TwilioClient
   end
 
   def notification_recipient_msg(user_id)
-    notification = ""
+    # notification = ""
     User.find(user_id).user_added_products.all.each do |user_added_product|
       user_added_product.recipients.all.each do |recipient|
-        send_text(recipient.phone_number, user_added_product.id)
-        send_text(User.find(user_id).phone_number, user_added_product.id)
+        send_text(user_added_product.id, recipient.id)
+        send_text_to_self(user_added_product.id)
       end
     end
   end
@@ -22,19 +22,27 @@ class TwilioClient
     Recipient.find(recipient_id).phone_number
   end
 
-  def send_text_to_self(user_added_product_id)
+  def self_phone_number(user_id)
+    User.find(user_id).phone_number
+  end
+
+  def message(user_added_product_id)
+    "This is your Perishab.ly alert. Your #{UserAddedProduct.find(user_added_product_id).name.downcase} will expire on #{UserAddedProduct.find(user_added_product_id).exp_date}."
+  end
+
+  def send_text_to_self(user_id, user_added_product_id)
     @client.account.sms.messages.create(
       :from => TWILIO_NUMBER,
-      :to => UserAddedProduct.find(user_added_product_id).user.phone_number,
-      :body => "This is your Perishab.ly alert. Your #{UserAddedProduct.find(user_added_product_id).name.downcase} will expire on #{UserAddedProduct.find(user_added_product_id).exp_date}."
+      :to => self_phone_number(user_id),
+      :body => message(user_added_product_id)
       )
   end
 
-  def send_text(user_added_product_id, recipient_id)
+  def send_text(recipient_id, user_added_product_id)
     @client.account.sms.messages.create(
       :from => TWILIO_NUMBER,
       :to => phone_number(recipient_id),
-      :body => "This is your Perishab.ly alert. Your #{UserAddedProduct.find(user_added_product_id).name.downcase} will expire on #{UserAddedProduct.find(user_added_product_id).exp_date}."
+      :body => message(user_added_product_id)
       )
   end
 end
--- lib/tasks/twilio.rake
+++ lib/tasks/twilio.rake
@@ -1,14 +1,13 @@
 namespace :twilio do
-	desc "SEND_TEXT"
-	task :send_text => :environment do
-		UserAddedProduct.all.each do |p|
-			p.recipients.each do |r|
-				if (p.notification_date.year == DateTime.now.year) && (p.notification_date.month == DateTime.now.month) && (p.notification_date.day == DateTime.now.day)
-					twilio_text = TwilioClient.new
-					twilio_text.send_text(p.id, r.id)
-					twilio_text.send_text_to_self(p.id)
-				end
-			end
-		end
-	end
+    desc "SEND_TEXT"
+    task :send_text => :environment do
+        twilio_text = TwilioClient.new
+        UserAddedProduct.all.each do |p|
+            p.relevant?
+            twilio_text.send_text_to_self(p.user_id, p.id)
+            p.recipients.all.each do |r|
+                twilio_text.send_text(r.id, p.id)
+            end
+        end
+    end
 end
